3a0d15ae41687837779a0806ce45cd366989f097
Manoj Rawat <123074273+manojrawat123@users.noreply.github.com>
2024-05-27 16:26:35 +0530

    Update settings.py

--- new_intern_project/settings.py
+++ new_intern_project/settings.py
@@ -175,8 +175,6 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER ='otp@simply2cloud.com'
-EMAIL_HOST_PASSWORD = "dsveoqolbfkuporu"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 PASSWORD_RESET_TIMEOUT=900        
